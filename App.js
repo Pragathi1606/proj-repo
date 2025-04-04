d08fbd894dde61371102dfc46fe03ef599c110fe
Pragathi1606 <krishtipatiharish@gmail.com>
2025-04-04 21:30:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome to React</h1>
+        <p>Tap here to open </p>
+      <a href="/proj.html" target="_blank" rel="noopener noreferrer">
+        Open Web page
+      </a>
+        
+      </header>
+      
+      
+    </div>
+  );
+}
+
+export default App;
